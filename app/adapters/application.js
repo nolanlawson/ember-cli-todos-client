262d9e4c61bb688e83aab124125eb835de37757e
Nolan Lawson <nolan.lawson@gmail.com>
2014-12-08 00:21:15 -0500

    better syncing

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -4,6 +4,16 @@ import DS from 'ember-data';
   host: 'http://localhost:1337'
 });*/
 
+var remote = new PouchDB('http://localhost:5984/ember-todos');
+var db = new PouchDB('todos');
+
+function doSync() {
+  db.sync(remote, {live: true}).on('error', function (err) {
+    setTimeout(doSync, 1000); // retry
+  });
+}
+doSync();
+
 export default EmberPouch.Adapter.extend({
-  db: new PouchDB('http://localhost:5984/foobarbazman')
+  db: db
 });
